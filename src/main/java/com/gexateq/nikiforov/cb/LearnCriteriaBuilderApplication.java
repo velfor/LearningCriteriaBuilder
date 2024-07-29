37563bb65992d0ef467846a99348e5e79f21f690
Pavel Nikiforov <velfor@tut.by>
2024-07-29 19:41:03 +0300

    Initial commit

---
+++ src/main/java/com/gexateq/nikiforov/cb/LearnCriteriaBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.gexateq.nikiforov.cb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnCriteriaBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnCriteriaBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gexateq/nikiforov/cb/LearnCriteriaBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gexateq.nikiforov.cb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnCriteriaBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
